5346883ec7ac2ca7094a2995a2cbac784906c026
padeler <padeler@ics.forth.gr>
2024-04-11 09:42:29 +0300

    ...

--- scratch/dust3r_to_colmap.py
+++ scratch/dust3r_to_colmap.py
@@ -38,7 +38,16 @@ model = load_model(weights, device)
 
 # filelist = ["../res/00001.png", "../res/00037.png"]
 # glob file list from folder
-filelist = glob.glob("../res/step64/*.png")
+# filelist = glob.glob("../res/step64/*.png")
+filelist = glob.glob("../res/2frames/*.png")
+# filelist = glob.glob("../res/16frames/*.png")
+# filelist = glob.glob("../res/undistorted/2frames/*.png")
+# filelist = glob.glob("../res/undistorted/3frames/*.png")
+# filelist = glob.glob("../res/undistorted/3near/*.png")
+# filelist = glob.glob("../res/undistorted/2near/*.png")
+# filelist = glob.glob("../res/undistorted/2frames_more/*.png")
+# exp_name = "2near_more"
+# filelist = glob.glob(f"../res/undistorted/{exp_name}/*.png")
 print(filelist)
 
 # %%
@@ -52,18 +61,62 @@ if len(imgs) == 1:
 # elif scenegraph_type == "oneref":
 #     scenegraph_type = scenegraph_type + "-" + str(refid)
 
+# %%
 scenegraph_type = "complete"
 
 pairs = make_pairs(imgs, scene_graph=scenegraph_type, prefilter=None, symmetrize=True)
 
 output = inference(pairs, model, device, batch_size=batch_size)
 
+# output is a dictionary with keys: view1, view2, pred1, pred2, loss (if criterion is not None)
+# view1 and view2 are the input images
+# pred1 and pred2 are the predicted vit outputs
+
+# %%
+
 mode = (
     GlobalAlignerMode.PointCloudOptimizer
     if len(imgs) > 2
     else GlobalAlignerMode.PairViewer
 )
-scene = global_aligner(output, device=device, mode=mode)
+scene = global_aligner(output, device=device, mode=mode)  # , optimize_pp=True)
+print(f"{type(scene)=}")
+# %%
+# 1 PINHOLE 1909 1075 1034.641136328017 1032.8412864854072 951.52624863191704 538.79198968529704
+# w, h, fx, fy, cx, cy = (
+#     1909,
+#     1075,
+#     1034.641136328017,
+#     1032.8412864854072,
+#     951.52624863191704,
+#     538.79198968529704,
+# )
+
+# long_edge_size = 512
+# S = max(w, h)
+
+# scale = long_edge_size / S
+# print(f"{scale=}")
+
+# new_focal = fx * scale
+# new_cx = cx * scale - 0.5
+# new_cy = cy * scale - 0.5
+
+# print("new_focal", new_focal)
+# print(f"{new_cx=} {new_cy=}")
+
+# focals = [new_focal] * 3
+# pps = [np.array([new_cx, new_cy])] * 3
+
+# print(scene.get_principal_points())
+# print(scene.pp)
+
+# scene.preset_focal(focals)
+# scene.preset_principal_point(pps)
+
+# print(scene.get_principal_points())
+# print(scene.get_focals())
+# %%
 lr = 0.01
 
 if mode == GlobalAlignerMode.PointCloudOptimizer:
@@ -72,13 +125,18 @@ if mode == GlobalAlignerMode.PointCloudOptimizer:
     )
     print(f"Loss = {loss}")
 
+print("Cleanining pointcloud")
+scene = scene.clean_pointcloud()
+
 print("done")
 
 # %%
 # convert scene to colmap format
 print("Saving frames")
-output_folder = "../res/colmap_out"
-os.makedirs(output_folder, exist_ok=True)
+output_folder = f"../res/colmap_out/undistorted/{exp_name}"
+sparse_folder = os.path.join(output_folder, "sparse", "0")
+os.makedirs(sparse_folder, exist_ok=True)
+
 
 # save images
 image_folder = os.path.join(output_folder, "images")
@@ -116,11 +174,12 @@ cameras = {
     for idx, (f, pp) in enumerate(zip(focals, pps))
 }
 
-CM.write_cameras_text(cameras, os.path.join(output_folder, "cameras.txt"))
+
+CM.write_cameras_text(cameras, os.path.join(sparse_folder, "cameras.txt"))
 
 # %%
 # Create points3D.txt
-
+print("Creating points3D.txt")
 imgs = scene.imgs
 # 3D pointcloud from depthmap, poses and intrinsics
 pts3d = to_numpy(scene.get_pts3d())
@@ -146,7 +205,7 @@ for idx, (pt, rgb) in tqdm(
     )
 
 
-CM.write_points3D_text(points3d, os.path.join(output_folder, "points3D.txt"))
+CM.write_points3D_text(points3d, os.path.join(sparse_folder, "points3D.txt"))
 
 # %%
 
@@ -156,6 +215,7 @@ poses = scene.get_im_poses().cpu().detach().numpy()
 
 images = {}
 for idx, p in tqdm(enumerate(poses), total=len(poses), desc="Creating images.txt"):
+    # print(f"{idx} {p=}")
     p = np.linalg.inv(p)
     images[idx] = CM.Image(
         id=idx,
@@ -167,6 +227,57 @@ for idx, p in tqdm(enumerate(poses), total=len(poses), desc="Creating images.txt
         point3D_ids=np.array([]),  # range(1000)),
     )
 
-CM.write_images_text(images, os.path.join(output_folder, "images.txt"))
+CM.write_images_text(images, os.path.join(sparse_folder, "images.txt"))
+
+
+# %%
+print(f"total points {len(points3d)}")
+p3d = np.empty((len(points3d), 4), dtype=np.float32)
+p3dcol = np.empty((len(points3d), 3), dtype=np.uint8)
+
+for idx, p in tqdm(points3d.items(), total=len(points3d), desc="converting to numpy"):
+    p3d[idx, :3] = p.xyz
+    p3d[idx, 3] = 1.0
+    p3dcol[idx, :] = p.rgb
+
+# %%
+cam_idx = 1
+
+cam = cameras[cam_idx]
+width, height = cam.width, cam.height
+fx, fy, cx, cy = cam.params
+K = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
+
+
+Q = images[cam_idx].qvec
+T = images[cam_idx].tvec
+R = CM.qvec2rotmat(Q)
+
+print("R ", R)
+RT = np.r_[np.c_[R, T], [[0, 0, 0, 1]]]
+
+print(f"{K=}")
+print(f"{RT=}")
+# %%
+
+# Use RT and K to project 3D points to 2d image
+p3dt = p3d.T
+p3dt = np.dot(RT, p3dt)
+p3dt = np.dot(K, p3dt[:3, :])
+p3dt[0, :] /= p3dt[2, :]
+p3dt[1, :] /= p3dt[2, :]
+
+# %%
+# Ok now draw the points on an image using their colors
+img = np.zeros((height, width, 3), dtype=np.uint8)
+for p, c in zip(p3dt.T, p3dcol):
+    x, y, z = p
+    if x >= 0 and y >= 0 and x < width and y < height and z > 0:
+        img[int(y), int(x)] = c
+
+# %%
+# show image
+plt.imshow(img)
+
 
 # %%
